b0a32a8e5bddcec912a63c3a012cbc0bf1fef1c5
Taylor Nelms <taylornelms15@gmail.com>
2022-08-29 07:05:16 -0400

    Created better kdtree query accessor, put example usage in main function
    Signed-off-by: Taylor Nelms <taylornelms15@gmail.com>

--- src/ThreadTree.py
+++ src/ThreadTree.py
@@ -30,6 +30,14 @@ class ThreadTree:
             self.entrylist.append(entry)
         kdtree_data = np.array([x.getLUV() for x in self.entrylist]) 
         self.kdtree = KDTree(kdtree_data)
+    
+    def getClosestEntry(self, luv_tuple):
+        """
+        Queries the internal kdtree for the closest thread to the given LUV tuple
+        Returns both the distance and the ThreadEntry that's closest
+        """
+        dist, idx = self.kdtree.query(luv_tuple)
+        return (dist, self.entrylist[idx])
 
 def main():
     """
@@ -37,6 +45,8 @@ def main():
     """
     infile = "../data/dmc_readable.parquet"
     mytree = ThreadTree(infile)
+    testColor = np.array([64, 128, 128])
+    print("TestColor dist %s away from thread %s" % mytree.getClosestEntry(testColor))
 
 if __name__ == "__main__":
     main()
